119178ee28d52a3ee13a4ab0851733d5aff89f54
Anil Kumar Shaw <anilshaw025@gmail.com>
2020-05-28 00:53:15 +0530

    ready

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
+const port = process.env || 3000;
 
 
 const weatherData = require('../utils/weatherData');
@@ -84,4 +85,4 @@ app.use('*', (req, res) => {
 
 
 
-app.listen(3000);
+app.listen(port);
